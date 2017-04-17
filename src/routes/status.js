52b131199da8111baaa1d40f9af23d63f2ee4ab7
Arron <arronhunt.mac@gmail.com>
2017-04-17 13:06:39 -0700

    Token now expeted to be sent as a header in the POST request

--- src/routes/status.js
+++ src/routes/status.js
@@ -10,23 +10,29 @@ router.get('/', function(req, res, next) {
 
 let server =    conf.get('server');
 let endpoint =  conf.get('endpoint');
-let token =     conf.get('token');
 
 router.post('/', function(req, res) {
-    if (req.body.status_text && req.body.status_emoji) {
 
-        let encodedBody = encodeURIComponent(JSON.stringify(req.body))
-        let url = server+endpoint+"?token="+token+"&profile="+encodedBody;
+    if (req.headers['token']) {
+        if (req.body.status_text && req.body.status_emoji) {
 
-        request.get(url, function(err, httpResponse, body) {
-            res.send({
-                "error": err,
-                "body": JSON.parse(body),
+            let token = req.headers['token'];
+            let encodedBody = encodeURIComponent(JSON.stringify(req.body))
+            let url = server+endpoint+"?token="+token+"&profile="+encodedBody;
+
+            request.get(url, function(err, httpResponse, body) {
+                res.send({
+                    "error": err,
+                    "body": JSON.parse(body),
+                })
             })
-        })
+        } else {
+            res.send({error: "Invalid body sent"});
+        }
     } else {
-        res.send({error: "Invalid body sent"});
+        res.send({error: "Missing token..."});
     }
+
 })
 
 module.exports = router;
